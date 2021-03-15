47e100ce7ef7ed53c140185f1378ed82104a64d0
andrea <andrecasa91@gmail.com>
2021-03-16 00:25:43 +0100

    debugging linked strategy

--- MT-RRT/src/strategies/linkedTrees/LinkedTreesStrategy.cpp
+++ MT-RRT/src/strategies/linkedTrees/LinkedTreesStrategy.cpp
@@ -22,17 +22,16 @@ namespace mt::solver {
 #pragma omp parallel \
 num_threads(static_cast<int>(Threads))
         {
-            E* Solver_to_use = &battery[omp_get_thread_num()];
-            for (size_t k = 0; k < iterations; k += Batch_size * Threads) {
-                if (!life) break;
-                Solver_to_use->extend(Batch_size);
+             E* Solver_to_use = &battery[omp_get_thread_num()];
+             for (size_t k = 0; k < iterations; k += Batch_size * Threads) {
+                 Solver_to_use->extend(Batch_size);
                 if (!Solver_to_use->getSolutions().empty() && (!Solver_to_use->isCumulating())) {
                     life = false;
                 }
 #pragma omp barrier
-                int ttt = omp_get_thread_num();
                 gatherer();
-            }
+                if (!life) break;
+             }
         }
         gatherer();
     }
