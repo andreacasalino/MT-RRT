a0f1555f51766787ca9c04c328cc54d1987955e6
andrea <andrecasa91@gmail.com>
2021-03-21 17:15:44 +0100

    debug sample navigation improved

--- Samples/03-Navigation/SampleNavigation-00-traj-gen.cpp
+++ Samples/03-Navigation/SampleNavigation-00-traj-gen.cpp
@@ -11,53 +11,69 @@
 #include <math.h>
 using namespace std;
 
-mt::sample::arrayJSON convert(const mt::NodeState& state) {
-    mt::sample::arrayJSON result;
-    for(std::size_t k=0; k<state.size(); ++k) {
-        result.addElement(mt::sample::Number<float>(state[k]));
-    }
-    return result;
+
+class TrajectoryGenLog {
+public:
+    TrajectoryGenLog() {
+        auto data = mt::sample::importNavigationProblem(std::string(CONFIG_FOLDER) + "/Sample00-config");
+        this->problem = std::move(std::get<0>(data));
+	    this->problemLog = dynamic_cast<const mt::sample::SampleDescription<mt::sample::Description>*>(problem->getTrajManager())->logDescription();
+    };
+    ~TrajectoryGenLog() {
+        mt::sample::structJSON log;
+        log.addElement("problem", this->problemLog);
+        log.addElement("traj", this->trajLog);
+        mt::sample::printData(log , "TrajGen.json");
+    };
+
+    inline mt::sampling::Sampler* getSampler() const { return this->problem->getSampler(); };
+
+    void addTrajectory(const mt::NodeState& start, const mt::NodeState& end) {
+        mt::sample::structJSON trjLogNew;
+        trjLogNew.addElement("start", convert(start));
+        trjLogNew.addElement("end", convert(end));
+        mt::sample::arrayJSON statesLog;
+        auto trj = problem->getTrajManager()->getTrajectory(start, end);
+        if(nullptr != trj) {
+            do {
+                statesLog.addElement(convert(trj->getCursor(), trj->getCumulatedCost()));
+            } while (trj->advance() == mt::traj::AdvanceInfo::advanced);
+            statesLog.addElement(convert(trj->getCursor(), trj->getCumulatedCost()));
+        }
+        trjLogNew.addElement("states", statesLog);
+        this->trajLog.addElement(trjLogNew);
+    };
+
+private:
+    static mt::sample::arrayJSON convert(const mt::NodeState& state) {
+        mt::sample::arrayJSON result;
+        for(std::size_t k=0; k<state.size(); ++k) {
+            result.addElement(mt::sample::Number<float>(state[k]));
+        }
+        return result;
+    };
+    static mt::sample::arrayJSON convert(const mt::NodeState& state, const float& cost) {
+        mt::sample::arrayJSON result = convert(state);
+        result.addElement(mt::sample::Number<float>(cost));
+        return result;
+    };
+
+    mt::ProblemPtr problem;
+    mt::sample::structJSON problemLog;
+    mt::sample::arrayJSON trajLog;
 };
 
 int main() {
-    mt::ProblemPtr problem;
-    {
-        auto data = mt::sample::importNavigationProblem(std::string(CONFIG_FOLDER) + "/Sample00-config");
-        problem = std::move(std::get<0>(data));
-    }
+    TrajectoryGenLog logger;
 
-    mt::sample::arrayJSON trajGenLog;
-    auto addTraj = [&trajGenLog, &problem](const mt::NodeState& start, const mt::NodeState& end){
-        mt::sample::structJSON trjLog;
-        trjLog.addElement("start", convert(start));
-        trjLog.addElement("end", convert(end));
-        mt::sample::arrayJSON statesLog;
-        {
-            auto trj = problem->getTrajManager()->getTrajectory(start, end);
-            if(nullptr != trj) {
-                do {
-                    statesLog.addElement(convert(trj->getCursor()));
-                } while (trj->advance() == mt::traj::AdvanceInfo::advanced);
-                statesLog.addElement(convert(trj->getCursor()));
-            }
-        }
-        trjLog.addElement("states", statesLog);
-        trajGenLog.addElement(trjLog);
-    };
-
-    addTraj(std::vector<float>{0.f, 0.f, 0.f}, std::vector<float>{100.f, 100.f, 0.5f * M_PI});
-    addTraj(std::vector<float>{0.f, 0.f, 0.f}, std::vector<float>{100.f, 100.f, 3.f * M_PI / 4.f});
-    // addTraj(std::vector<float>{80.f, 0.f, 0.f}, std::vector<float>{100.f, 100.f, 0.5f * M_PI});
-    // addTraj(std::vector<float>{0.f, 0.f, 0.f}, std::vector<float>{200.f, 100.f, 0.25f * M_PI});
-    // addTraj(std::vector<float>{0.f, 0.f, -0.25f * M_PI}, std::vector<float>{200.f, 100.f, 0.25f * M_PI});
-    // for(std::size_t k=0; k<20; ++k) {
-    //     addTraj(problem->getSampler()->randomState() , problem->getSampler()->randomState());
+    logger.addTrajectory(std::vector<float>{0.f, 0.f, 0.f}, std::vector<float>{100.f, 100.f, 0.5f * M_PI});
+    // logger.addTrajectory(std::vector<float>{0.f, 0.f, 0.f}, std::vector<float>{100.f, 100.f, 3.f * M_PI / 4.f});
+    // logger.addTrajectory(std::vector<float>{80.f, 0.f, 0.f}, std::vector<float>{100.f, 100.f, 0.5f * M_PI});
+    // logger.addTrajectory(std::vector<float>{0.f, 0.f, 0.f}, std::vector<float>{200.f, 100.f, 0.25f * M_PI});
+    // logger.addTrajectory(std::vector<float>{0.f, 0.f, -0.25f * M_PI}, std::vector<float>{200.f, 100.f, 0.25f * M_PI});
+    // for(std::size_t k=0; k<5; ++k) {
+    //     logger.addTrajectory(logger.getSampler()->randomState() , logger.getSampler()->randomState());
     // }
-
-    mt::sample::structJSON log;
-	log.addElement("problem", dynamic_cast<const mt::sample::SampleDescription<mt::sample::Description>*>(problem->getTrajManager())->logDescription());
-    log.addElement("traj", trajGenLog);
-    mt::sample::printData(log , "TrajGen.json");
 
     return EXIT_SUCCESS;
 }
