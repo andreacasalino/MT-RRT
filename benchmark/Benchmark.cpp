763e82dc2511b1d067a4920e0ff185654884de47
Foo <foo:gmail.com>
2024-11-03 19:47:34 +0100

    impl

--- benchmark/Benchmark.cpp
+++ benchmark/Benchmark.cpp
@@ -41,13 +41,12 @@ ExpansionStrategy toStrategy(std::int64_t val) {
   return res;
 }
 
-template<typename PlannerT>
-struct Benchmark {};
+template <typename PlannerT> struct Benchmark {};
 
-template<typename PlannerT>
-struct BenchmarkBase {
+template <typename PlannerT> struct BenchmarkBase {
   virtual void setUp(benchmark::State &state) {
-    ExtendProblem data = make_scenario(toKind(state.range(0)), toStrategy(state.range(1)));
+    ExtendProblem data =
+        make_scenario(toKind(state.range(0)), toStrategy(state.range(1)));
 
     problem = data.point_problem;
     start = data.start.asView().convert();
@@ -55,7 +54,7 @@ struct BenchmarkBase {
     pars = std::move(data.suggested_parameters);
     pars.dumpTrees = false;
     pars.best_effort = false;
-    pars.iterations.set( state.range(2) );
+    pars.iterations.set(state.range(2));
   }
 
   void run(benchmark::State &state) {
@@ -77,15 +76,14 @@ struct BenchmarkBase {
 struct Args {
   using ParsPack = std::vector<std::vector<std::int64_t>>;
 
-  static const inline ParsPack standard = ParsPack{{0,1,2}, {0,1}, {100, 200, 500, 1000, 2000, 5000}};
-
+  static const inline ParsPack standard =
+      ParsPack{{0, 1, 2}, {0, 1}, {100, 200, 500, 1000, 2000, 5000}};
 
   struct ParsPackMerge {
     ParsPackMerge() = default;
-    ParsPackMerge(const ParsPack& begin) 
-    : res{begin} {}
+    ParsPackMerge(const ParsPack &begin) : res{begin} {}
 
-    ParsPackMerge& add(std::vector<std::int64_t>&& to_add) {
+    ParsPackMerge &add(std::vector<std::int64_t> &&to_add) {
       res.emplace_back(std::forward<std::vector<std::int64_t>>(to_add));
       return *this;
     }
@@ -93,27 +91,25 @@ struct Args {
     ParsPack res;
   };
 
-  static const inline ParsPack multiThreaded = ParsPackMerge{standard}.add({2, 3, 4, 6, 8}).res;
+  static const inline ParsPack multiThreaded =
+      ParsPackMerge{standard}.add({2, 3, 4, 6, 8}).res;
 };
 
-template<>
-struct Benchmark<StandardPlanner>
-: public benchmark::Fixture
-, public BenchmarkBase<StandardPlanner> {
+template <>
+struct Benchmark<StandardPlanner> : public benchmark::Fixture,
+                                    public BenchmarkBase<StandardPlanner> {
   void setUp(benchmark::State &state) override {
     this->BenchmarkBase<StandardPlanner>::setUp(state);
     this->planner.emplace(std::move(*this->problem));
   }
 };
 
-BENCHMARK_TEMPLATE_F(Benchmark, StandardTest, StandardPlanner)(benchmark::State& st) {
-  this->run(st);
-}
+BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, StandardTest, StandardPlanner)
+(benchmark::State &st) { this->run(st); }
 BENCHMARK_REGISTER_F(Benchmark, StandardTest)->ArgsProduct(Args::standard);
 
-template<typename PlannerT>
-struct BenchmarkMultiThreaded
-: public BenchmarkBase<PlannerT> {
+template <typename PlannerT>
+struct BenchmarkMultiThreaded : public BenchmarkBase<PlannerT> {
   void setUp(benchmark::State &state) override {
     this->BenchmarkBase<PlannerT>::setUp(state);
     this->planner.emplace(std::move(*this->problem));
@@ -121,57 +117,57 @@ struct BenchmarkMultiThreaded
   }
 };
 
-template<>
+template <>
 struct Benchmark<EmbarassinglyParallelPlanner>
-: public benchmark::Fixture
-, public BenchmarkMultiThreaded<EmbarassinglyParallelPlanner> {};
-BENCHMARK_TEMPLATE_F(Benchmark, EmbarassinglyParallelTest, EmbarassinglyParallelPlanner)(benchmark::State& st) {
-  this->run(st);
-}
-BENCHMARK_REGISTER_F(Benchmark, EmbarassinglyParallelTest)->ArgsProduct(Args::multiThreaded);
+    : public benchmark::Fixture,
+      public BenchmarkMultiThreaded<EmbarassinglyParallelPlanner> {};
+BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, EmbarassinglyParallelTest,
+                            EmbarassinglyParallelPlanner)
+(benchmark::State &st) { this->run(st); }
+BENCHMARK_REGISTER_F(Benchmark, EmbarassinglyParallelTest)
+    ->ArgsProduct(Args::multiThreaded);
 
-template<>
+template <>
 struct Benchmark<ParallelizedQueriesPlanner>
-: public benchmark::Fixture
-, public BenchmarkMultiThreaded<ParallelizedQueriesPlanner> {};
-BENCHMARK_TEMPLATE_F(Benchmark, ParallelizedQueriesTest, ParallelizedQueriesPlanner)(benchmark::State& st) {
-  this->run(st);
-}
-BENCHMARK_REGISTER_F(Benchmark, ParallelizedQueriesTest)->ArgsProduct(Args::multiThreaded);
+    : public benchmark::Fixture,
+      public BenchmarkMultiThreaded<ParallelizedQueriesPlanner> {};
+BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, ParallelizedQueriesTest,
+                            ParallelizedQueriesPlanner)
+(benchmark::State &st) { this->run(st); }
+BENCHMARK_REGISTER_F(Benchmark, ParallelizedQueriesTest)
+    ->ArgsProduct(Args::multiThreaded);
 
-template<>
+template <>
 struct Benchmark<SharedTreePlanner>
-: public benchmark::Fixture
-, public BenchmarkMultiThreaded<SharedTreePlanner> {};
-BENCHMARK_TEMPLATE_F(Benchmark, SharedTreeTest, SharedTreePlanner)(benchmark::State& st) {
-  this->run(st);
-}
-BENCHMARK_REGISTER_F(Benchmark, SharedTreeTest)->ArgsProduct(Args::multiThreaded);
+    : public benchmark::Fixture,
+      public BenchmarkMultiThreaded<SharedTreePlanner> {};
+BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, SharedTreeTest, SharedTreePlanner)
+(benchmark::State &st) { this->run(st); }
+BENCHMARK_REGISTER_F(Benchmark, SharedTreeTest)
+    ->ArgsProduct(Args::multiThreaded);
 
-template<>
+template <>
 struct Benchmark<LinkedTreesPlanner>
-: public benchmark::Fixture
-, public BenchmarkMultiThreaded<LinkedTreesPlanner> {};
-BENCHMARK_TEMPLATE_F(Benchmark, LinkedTreesTest, LinkedTreesPlanner)(benchmark::State& st) {
-  this->run(st);
-}
-BENCHMARK_REGISTER_F(Benchmark, LinkedTreesTest)->ArgsProduct(Args::multiThreaded);
+    : public benchmark::Fixture,
+      public BenchmarkMultiThreaded<LinkedTreesPlanner> {};
+BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, LinkedTreesTest, LinkedTreesPlanner)
+(benchmark::State &st) { this->run(st); }
+BENCHMARK_REGISTER_F(Benchmark, LinkedTreesTest)
+    ->ArgsProduct(Args::multiThreaded);
 
-template<>
+template <>
 struct Benchmark<MultiAgentPlanner>
-: public benchmark::Fixture
-, public BenchmarkMultiThreaded<MultiAgentPlanner> {
+    : public benchmark::Fixture,
+      public BenchmarkMultiThreaded<MultiAgentPlanner> {
   void setUp(benchmark::State &state) override {
     this->BenchmarkMultiThreaded<MultiAgentPlanner>::setUp(state);
     this->planner->synchronization().set(0.1f);
   }
 };
-BENCHMARK_TEMPLATE_F(Benchmark, MultiAgentTest, MultiAgentPlanner)(benchmark::State& st) {
-  this->run(st);
-}
-BENCHMARK_REGISTER_F(Benchmark, MultiAgentTest)->ArgsProduct(Args::multiThreaded);
-
-
+BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, MultiAgentTest, MultiAgentPlanner)
+(benchmark::State &st) { this->run(st); }
+BENCHMARK_REGISTER_F(Benchmark, MultiAgentTest)
+    ->ArgsProduct(Args::multiThreaded);
 
 // Run the benchmark
 BENCHMARK_MAIN();
