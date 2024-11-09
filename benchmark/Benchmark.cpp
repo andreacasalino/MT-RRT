84ff7e97b2634d220c562e5d0e15322cd9a8a856
Foo <foo:gmail.com>
2024-11-09 21:46:18 +0100

    impl

--- benchmark/Benchmark.cpp
+++ benchmark/Benchmark.cpp
@@ -48,7 +48,51 @@ ExpansionStrategy toStrategy(std::int64_t val) {
   return res;
 }
 
-/*
+struct Args {
+  Args() = default;
+
+  Args &add(std::vector<std::int64_t> pack) {
+    args_.emplace_back(std::move(pack));
+    return *this;
+  }
+
+  Args &iterations(int iters) {
+    iterations_ = iters;
+    return *this;
+  }
+
+  template <typename Pred> void forEach(Pred pred) const {
+    this->forEach_(std::vector<std::int64_t>{}, args_.begin(), pred);
+  }
+
+private:
+  template <typename Pred>
+  void forEach_(std::vector<std::int64_t> cumulated,
+                std::vector<std::vector<std::int64_t>>::iterator remainingIt,
+                const Pred &pred) const {
+    if (remainingIt == args_.end()) {
+      for (int i = 0; i < iterations_; ++i) {
+        pred(cumulated, i + 1, iterations_);
+      }
+      return;
+    }
+    for (auto val : *remainingIt) {
+      auto cumulated_next = cumulated;
+      cumulated_next.push_back(val);
+      forEach_(std::move(cumulated_next), remainingIt + 1, pred);
+    }
+  }
+
+  std::vector<std::vector<std::int64_t>> args_;
+  int iterations_{20};
+};
+
+const Args single_threaded_args = Args{}.add({0, 1, 2}).add({0, 1}).add(
+    {100, 200, 500, 1000, 2000, 5000, 10000});
+
+const Args multi_threaded_args =
+    Args{single_threaded_args}.add({2, 3, 4, 6, 8});
+
 struct Logger {
 public:
   ~Logger() {
@@ -62,11 +106,11 @@ public:
   }
 
   struct Record {
-    Record(Logger &src, benchmark::State &state, int argsNumb)
-        : source_{src}, label_{state.name()} {
-      for (int k = 0; k < argsNumb; ++k) {
+    Record(const std::vector<std::int64_t> &args)
+        : source_{Logger::get()}, label_{} {
+      for (auto val : args) {
         label_ += '-';
-        label_ += std::to_string(state.range(k));
+        label_ += std::to_string(val);
       }
     }
 
@@ -85,195 +129,84 @@ public:
     std::string label_;
   };
 
-  template <typename... ARGS>
-  std::unique_ptr<Record> makeRecord(ARGS &&...args) {
-    return std::make_unique<Record>(*this, std::forward<ARGS>(args)...);
-  }
-
 private:
   Logger() = default;
 
   nlohmann::json data;
 };
-*/
 
-struct Args {
-  Args() = default;
+template <typename PlannerT> struct BenchmarkContext {};
 
-  Args &add(std::vector<std::int64_t> pack) {
-    args_.emplace_back(std::move(pack));
-    return *this;
-  }
-
-  Args &iterations(int iters) {
-    iterations_ = iters;
-    return *this;
-  }
-
-  template <typename Pred> void forEach(Pred pred) {
-    this->forEach_(std::vector<std::int64_t>{}, args_.begin(), pred);
-  }
-
-private:
-  template <typename Pred>
-  void forEach_(std::vector<std::int64_t> cumulated,
-                std::vector<std::vector<std::int64_t>>::iterator remainingIt,
-                const Pred &pred) {
-    if (remainingIt == args_.end()) {
-      for (int i = 0; i < iterations_; ++i) {
-        pred(cumulated);
-      }
-      return;
-    }
-    for (auto val : *remainingIt) {
-      auto cumulated_next = cumulated;
-      cumulated_next.push_back(val);
-      forEach_(std::move(cumulated_next), remainingIt + 1, pred);
-    }
-  }
-
-  std::vector<std::vector<std::int64_t>> args_;
-  int iterations_{20};
-};
-
-TEST(Foo, Bla) {
-  Args{}
-      .add(std::vector<std::int64_t>{0, 1})
-      .add(std::vector<std::int64_t>{3, 4, 5})
-      .add(std::vector<std::int64_t>{0, 7})
-      .forEach([](const std::vector<std::int64_t> &args) {
-        for (auto val : args) {
-          std::cout << ' ' << val;
-        }
-        std::cout << std::endl;
-      });
-}
-
-/*
-template <typename PlannerT> struct Benchmark {};
-
-template <typename PlannerT> struct BenchmarkBase : public benchmark::Fixture {
-  virtual void setUp(benchmark::State &state) {
-    ExtendProblem data =
-        make_scenario(toKind(state.range(0)), toStrategy(state.range(1)));
-
-    problem = data.point_problem;
-    start = data.start.asView().convert();
-    end = data.end.asView().convert();
-    pars = std::move(data.suggested_parameters);
-    pars.dumpTrees = false;
-    pars.best_effort = false;
-    pars.iterations.set(state.range(2));
-  }
-
-  void run(benchmark::State &state) {
-    for (auto _ : state) {
-      state.PauseTiming();
-      this->setUp(state);
-      state.ResumeTiming();
-      auto durationGuard = Logger::get().makeRecord(state, this->ArgsCnt());
-      planner->solve(start, end, pars);
-    }
-  }
-
-  std::shared_ptr<ProblemDescription> problem;
-  std::vector<float> start;
-  std::vector<float> end;
-  Parameters pars;
-  std::optional<PlannerT> planner;
-};
-
-struct Args {
-  using ParsPack = std::vector<std::vector<std::int64_t>>;
-
-  static const inline ParsPack standard =
-      ParsPack{{0, 1, 2}, {0, 1}, {100, 200, 500, 1000, 2000, 5000, 10000}};
-
-  struct ParsPackMerge {
-    ParsPackMerge() = default;
-    ParsPackMerge(const ParsPack &begin) : res{begin} {}
-
-    ParsPackMerge &add(std::vector<std::int64_t> &&to_add) {
-      res.emplace_back(std::forward<std::vector<std::int64_t>>(to_add));
-      return *this;
-    }
-
-    ParsPack res;
-  };
-
-  static const inline ParsPack multiThreaded =
-      ParsPackMerge{standard}.add({2, 3, 4, 6, 8}).res;
-};
-
-template <>
-struct Benchmark<StandardPlanner> : public BenchmarkBase<StandardPlanner> {
-  void setUp(benchmark::State &state) override {
-    this->BenchmarkBase<StandardPlanner>::setUp(state);
-    this->planner.emplace(std::move(*this->problem));
+template <typename PlannerT> struct Benchmark : ::testing::Test {
+  void solve() {
+    BenchmarkContext<PlannerT>::args.forEach(
+        [](const std::vector<std::uint64_t> &parameters, int iter,
+           int iter_tot) {
+          ExtendProblem data =
+              make_scenario(toKind(state.range(0)), toStrategy(state.range(1)));
+          data.suggested_parameters.iterations.set(parameters[2]);
+          auto planner =
+              BenchmarkContext<PlannerT>::make(data.point_problem, parameters);
+          Logger::Record record{parameters};
+          planner->solve(data.start, data.end, data.suggested_parameters);
+        });
   }
 };
 
-BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, StandardTest, StandardPlanner)
-(benchmark::State &st) { this->run(st); }
-BENCHMARK_REGISTER_F(Benchmark, StandardTest)->ArgsProduct(Args::standard);
+template <> struct BenchmarkContext<StandardPlanner> {
+  static std::unique_ptr<StandardPlanner>
+  make(std::shared_ptr<ProblemDescription> problem,
+       const std::vector<std::uint64_t> &parameters) {
+    return std::make_unique<StandardPlanner>(std::move(*problem));
+  }
+
+  static inline const Args args = Args{}.add({0, 1, 2}).add({0, 1}).add(
+      {100, 200, 500, 1000, 2000, 5000, 10000});
+};
 
 template <typename PlannerT>
 struct BenchmarkMultiThreaded : public BenchmarkBase<PlannerT> {
-  void setUp(benchmark::State &state) override {
-    this->BenchmarkBase<PlannerT>::setUp(state);
-    this->planner.emplace(std::move(*this->problem));
-    this->planner->setThreads(Threads(state.range(3)));
+  static std::unique_ptr<PlannerT>
+  make(std::shared_ptr<ProblemDescription> problem,
+       const std::vector<std::uint64_t> &parameters) {
+    auto planner = std::make_unique<PlannerT>(std::move(*problem));
+    planner->setThreads(Threads(parameters.back()));
+    return planner;
   }
+
+  static inline const Args args =
+      Args{BenchmarkContext<StandardPlanner>::args}.add({2, 3, 4, 6, 8});
 };
 
 template <>
-struct Benchmark<EmbarassinglyParallelPlanner>
-    : public BenchmarkMultiThreaded<EmbarassinglyParallelPlanner> {};
-BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, EmbarassinglyParallelTest,
-                            EmbarassinglyParallelPlanner)
-(benchmark::State &st) { this->run(st); }
-BENCHMARK_REGISTER_F(Benchmark, EmbarassinglyParallelTest)
-    ->ArgsProduct(Args::multiThreaded);
+struct BenchmarkContext<EmbarassinglyParallelPlanner>
+    : BenchmarkMultiThreaded<EmbarassinglyParallelPlanner> {};
 
 template <>
-struct Benchmark<ParallelizedQueriesPlanner>
-    : public BenchmarkMultiThreaded<ParallelizedQueriesPlanner> {};
-BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, ParallelizedQueriesTest,
-                            ParallelizedQueriesPlanner)
-(benchmark::State &st) { this->run(st); }
-BENCHMARK_REGISTER_F(Benchmark, ParallelizedQueriesTest)
-    ->ArgsProduct(Args::multiThreaded);
+struct BenchmarkContext<ParallelizedQueriesPlanner>
+    : BenchmarkMultiThreaded<ParallelizedQueriesPlanner> {};
 
 template <>
-struct Benchmark<SharedTreePlanner>
-    : public BenchmarkMultiThreaded<SharedTreePlanner> {};
-BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, SharedTreeTest, SharedTreePlanner)
-(benchmark::State &st) { this->run(st); }
-BENCHMARK_REGISTER_F(Benchmark, SharedTreeTest)
-    ->ArgsProduct(Args::multiThreaded);
+struct BenchmarkContext<SharedTreePlanner>
+    : BenchmarkMultiThreaded<SharedTreePlanner> {};
 
 template <>
-struct Benchmark<LinkedTreesPlanner>
-    : public BenchmarkMultiThreaded<LinkedTreesPlanner> {};
-BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, LinkedTreesTest, LinkedTreesPlanner)
-(benchmark::State &st) { this->run(st); }
-BENCHMARK_REGISTER_F(Benchmark, LinkedTreesTest)
-    ->ArgsProduct(Args::multiThreaded);
+struct BenchmarkContext<LinkedTreesPlanner>
+    : BenchmarkMultiThreaded<LinkedTreesPlanner> {};
 
 template <>
-struct Benchmark<MultiAgentPlanner>
-    : public BenchmarkMultiThreaded<MultiAgentPlanner> {
-  void setUp(benchmark::State &state) override {
-    this->BenchmarkMultiThreaded<MultiAgentPlanner>::setUp(state);
-    this->planner->synchronization().set(0.1f);
+struct BenchmarkContext<MultiAgentPlanner>
+    : BenchmarkMultiThreaded<MultiAgentPlanner> {
+  static std::unique_ptr<MultiAgentPlanner>
+  make(std::shared_ptr<ProblemDescription> problem,
+       const std::vector<std::uint64_t> &parameters) {
+    auto planner =
+        BenchmarkMultiThreaded<MultiAgentPlanner>::make(problem, parameters);
+    planner->synchronization().set(0.1f);
+    return planner;
   }
 };
-BENCHMARK_TEMPLATE_DEFINE_F(Benchmark, MultiAgentTest, MultiAgentPlanner)
-(benchmark::State &st) { this->run(st); }
-BENCHMARK_REGISTER_F(Benchmark, MultiAgentTest)
-    ->ArgsProduct(Args::multiThreaded);
 
-// Run the benchmark
-BENCHMARK_MAIN();
-
-*/
+using BenchmarkTypes = testing::Types<StandardPlanner>;
+TYPED_TEST_SUITE(Benchmark, BenchmarkTypes);
+TYPED_TEST(Benchmark, profile) { this->solve(); }
