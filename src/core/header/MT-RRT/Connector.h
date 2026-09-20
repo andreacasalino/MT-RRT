68940187ee7edbf3573941fc300b0701e8375ca3
Foo <foo:gmail.com>
2026-09-20 20:21:49 +0200

    impl

--- src/core/header/MT-RRT/Connector.h
+++ src/core/header/MT-RRT/Connector.h
@@ -13,17 +13,17 @@
 #include <MT-RRT/Types.h>
 
 #include <optional>
+#include <span>
+#include <vector>
 
 namespace mt_rrt {
-
 /**
  * @brief Factory of optimal trajectories, refer to Section 1.3.0.1 of the
  * documentation.
  */
-class Connector : public Copiable<Connector> {
-public:
-  Connector(const Connector &) = delete;
-  Connector &operator=(const Connector &) = delete;
+template <typename C>
+concept Connector = requires(const C obj_const, std::span<const float> start,
+                             std::span<const float> target) {
 
   /**
    * @param the starting node in the trajectory whose cost is to evaluate
@@ -32,19 +32,7 @@ public:
    * optimal trajectory connecting the passed pair of states. This cost does not
    * acoount for constraints, as this is done by minCost2GoConstrained(...)
    */
-  virtual float minCost2Go(const View &start, const View &end) const = 0;
-
-  /**
-   * @param the starting node in the trajectory whose cost is to evaluate
-   * @param the ending node in the trajectory whose cost is to evaluate
-   * @return the cost C(\tau), Section 1.2 of the documentation, of
-   * the trajectory \tau going from the starting node to the ending one, but
-   * accounting for the constraints, Section 1.2 of the documentation.
-   * COST_MAX is returned in this case returned when a feasible trajectory
-   * exists, but is not entirely contained in the admitted set, Section 1.2 of
-   * the documentation.
-   */
-  float minCost2GoConstrained(const View &start, const View &end) const;
+  { obj_const.minCost2Go(start, target) } -> std::same_as<Positive>;
 
   /**
    * @param the starting state
@@ -52,27 +40,92 @@ public:
    * @return the optimal trajectory connecting the passed states. nullptr is
    * returned in case a feasible trajectory does not exist
    */
-  virtual TrajectoryPtr getTrajectory(const View &start,
-                                      const View &end) const = 0;
+  {
+    obj_const.getTrajectory(start, target)
+    } -> std::same_as<std::optional<typename C::Trajectory>>;
+}
+&&std::is_base_of_v<Copiable<C>, C>;
 
-  /**
-   * @brief Performs a steering operation, Sections 1.2.1, 1.2.2 and 1.2.3 of
-   * the documentation, from a staring node to a target one.
-   * @param starting configuration
-   * @param target configuration to reach
-   * @return the steered configuration and a flag specifying whether target was
-   * reached. Is a nullopt when the steering was not possible at all
-   */
-  struct SteerResult {
-    NodeOwning node;
-    bool target_is_reached;
-  };
-  std::optional<SteerResult> steer(const Node &start, const View &target,
-                                   const SteerIterations &trials) const;
+/**
+ * @param the connector
+ * @param the starting node in the trajectory whose cost is to evaluate
+ * @param the ending node in the trajectory whose cost is to evaluate
+ * @return the cost C(\tau), Section 1.2 of the documentation, of
+ * the trajectory \tau going from the starting node to the ending one, but
+ * accounting for the constraints, Section 1.2 of the documentation.
+ * COST_MAX is returned in this case returned when a feasible trajectory
+ * exists, but is not entirely contained in the admitted set, Section 1.2 of
+ * the documentation.
+ */
+template <Connector C>
+Positive minCost2GoConstrained(const C &connector, std::span<const float> start,
+                               std::span<const float> target) {
+  Positive distance = connector.minCost2Go(start, target);
+  if (COST_MAX == distance.get()) {
+    return distance;
+  }
 
-protected:
-  Connector() = default;
+  auto traj = connector.getTrajectory(start, target);
+  if (!traj.has_value()) {
+    throw Error{"found null trajectory but the min cost 2 go was not infinite"};
+  }
+  AdvanceInfo advInfo = traj.advance();
+  for (; std::holds_alternative<Advanced>(advInfo); advInfo = traj->advance()) {
+  }
+  return std::holds_alternative<BlockedTag>(advInfo) ? COST_MAX : distance;
+}
+
+struct SteerResult {
+  bool target_reached{false};
+  Positive cost2Go{0};
 };
 
-using ConnectorPtr = std::unique_ptr<Connector>;
+/**
+ * @return nullopt when no steer was actually possible
+ */
+template <Connector C>
+[[nodiscard]] std::optional<SteerResult>
+steer(const C &connector, std::vector<float> &reached_state,
+      std::span<const float> start, std::span<const float> target,
+      const SteerIterations &trials) {
+  reached_state.clear();
+  auto traj = connector.getTrajectory(start, target);
+  if (!traj.has_value()) [[unlikely]] {
+    return std::nullopt;
+  }
+
+  std::optional<SteerResult> res;
+  auto set_result_ = [&](std::span<const float> state, const Positive &c) {
+    reached_state.clear();
+    reached_state.insert(reached_state.end(), state.begin(), state.end());
+    res.emplace().cost2Go = c;
+  };
+
+  bool steered{false};
+  for (std::size_t t = 0; t < trials.get(); ++t) {
+    bool stop = std::visit(
+        [&](auto &&advancement) {
+          if constexpr (std::is_same_v<decltype(advancement), BlockedTag &&>) {
+            return true;
+          }
+
+          else if constexpr (std::is_same_v<decltype(advancement),
+                                            Advanced &&>) {
+            set_result_(advancement.reached_state, advancement.cumulatedCost);
+            return false;
+          }
+
+          else {
+            set_result_(target, advancement.cumulatedCost);
+            res->target_reached = true;
+            return true;
+          }
+        },
+        traj->advance());
+    if (stop) {
+      break;
+    }
+  }
+  return res;
+}
 } // namespace mt_rrt
