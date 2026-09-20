a20549cc0bf6a6773b9776eb5befdca953a2b8b7
Foo <foo:gmail.com>
2026-09-20 13:35:40 +0200

    impl

--- src/carpet/header/MT-RRT/ObjectPool.h
+++ src/carpet/header/MT-RRT/ObjectPool.h
@@ -7,88 +7,27 @@
 
 #pragma once
 
-#include <cstring>
-#include <memory>
-#include <type_traits>
-#include <vector>
+#include <deque>
+#include <span>
 
 namespace mt_rrt {
 template <typename T> class ObjectPool {
 public:
-  ObjectPool();
+  ObjectPool() = default;
 
-  template <typename... Args> T &emplace_back(Args &&...args);
+  ObjectPool(const ObjectPool &) = delete;
+  ObjectPool &operator=(const ObjectPool &) = delete;
 
-  T *emplace_back_multiple(const T *source, std::size_t how_many);
+  ObjectPool(ObjectPool &&) = delete;
+  ObjectPool &operator=(ObjectPool &&) = delete;
 
-  static const inline std::size_t INITIAL_CAPACITY = 100;
+  T &push(T &&to_add) { return pool_.emplace_back(std::forward<T>(to_add)); }
+
+  void push(std::span<const T> to_add) {
+    pool_.insert(pool_.end(), to_add.begin(), to_add.end());
+  }
 
 private:
-  struct Chunk {
-    Chunk(std::size_t capacity) : capacity_{capacity} {
-      buffer = new char[sizeof(T) * capacity];
-    }
-    ~Chunk() {
-      if constexpr (!(std::is_same_v<T, int> || std::is_same_v<T, float> ||
-                      std::is_same_v<T, double>)) // in principle others like
-                                                  // std::uintXX
-      {
-        T *ptr = reinterpret_cast<T *>(buffer);
-        for (std::size_t k = 0; k < size_; ++k, ++ptr) {
-          ptr->~T();
-        }
-      }
-      delete[] buffer;
-    }
-
-    T *at(std::size_t pos) {
-      T *ptr = reinterpret_cast<T *>(buffer);
-      return &ptr[pos];
-    }
-
-    std::size_t capacity_;
-    std::size_t size_ = 0;
-    char *buffer = nullptr;
-  };
-
-  std::vector<std::unique_ptr<Chunk>> chunks_;
+  std::deque<T> pool_;
 };
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-template <typename T> ObjectPool<T>::ObjectPool() {
-  chunks_.emplace_back(std::make_unique<Chunk>(INITIAL_CAPACITY));
-}
-
-template <typename T>
-template <typename... Args>
-T &ObjectPool<T>::emplace_back(Args &&...args) {
-  Chunk *recipient = chunks_.back().get();
-  if (recipient->size_ == recipient->capacity_) {
-    // create a new chunk
-    recipient =
-        chunks_.emplace_back(std::make_unique<Chunk>(recipient->capacity_ * 10))
-            .get();
-  }
-  T *slot = recipient->at(recipient->size_++);
-  new (slot) T{std::forward<Args>(args)...};
-  return *slot;
-}
-
-template <typename T>
-T *ObjectPool<T>::emplace_back_multiple(const T *source, std::size_t how_many) {
-  Chunk *recipient = chunks_.back().get();
-  if (std::size_t residual_size = recipient->capacity_ - recipient->size_;
-      residual_size < how_many) {
-    std::size_t next_chunk_cap =
-        std::max<std::size_t>(how_many, recipient->capacity_ * 10);
-    // create a new chunk
-    recipient =
-        chunks_.emplace_back(std::make_unique<Chunk>(next_chunk_cap)).get();
-  }
-  T *slot = recipient->at(recipient->size_);
-  std::memcpy(slot, source, how_many * sizeof(T));
-  recipient->size_ += how_many;
-  return slot;
-}
 } // namespace mt_rrt
