fd742a9643965c91cc885b81b24a42c0eb8494c2
Foo <foo:gmail.com>
2026-09-20 19:12:48 +0200

    impl

--- src/carpet/header/MT-RRT/Limited.h
+++ src/carpet/header/MT-RRT/Limited.h
@@ -12,36 +12,11 @@
 
 namespace mt_rrt {
 /**
- * @brief A quantity whose value should always remain between defined
- * bounds
+ * @brief A real number whose value should always be positive
  */
-template <typename T> class Limited {
+template <typename T, T LowerBound, T UpperBound> class Limited {
 public:
-  /** @param lower bound for the value
-   *  @param upper bound for the value
-   *  @param initial value to set
-   */
-  Limited(const T &lowerBound, const T &upperBound, const T &initialValue)
-      : lowerBound(lowerBound), upperBound(upperBound) {
-    if (this->lowerBound >= this->upperBound) {
-      throw Error("inconsistent bounds");
-    }
-    this->set(initialValue);
-  };
-
-  /** @brief similar to Limited::Limited(const T& lowerBound, const T&
-   * upperBound, const T& initialValue), assuming lowerBound as initial value
-   */
-  template <bool LowerOrUpper = true>
-  Limited(const T &lowerBound, const T &upperBound)
-      : Limited(lowerBound, upperBound,
-                LowerOrUpper ? lowerBound : upperBound){};
-
-  Limited(const Limited &) = default;
-  Limited &operator=(const Limited &) = default;
-
-  inline T getLowerBound() const { return this->lowerBound; };
-  inline T getUpperBound() const { return this->upperBound; };
+  Limited(T val) { set(val); };
 
   /** @return the current value
    */
@@ -51,49 +26,25 @@ public:
    *  @throw if the value is not consistent with the bounds
    */
   void set(const T &newValue) {
-    if (newValue < this->lowerBound) {
-      throw Error::make("value is lower than minum which is {}", lowerBound);
+    if constexpr (std::numeric_limits<T>::min() < LowerBound) {
+      if (newValue < LowerBound) {
+        throw Error::make("value is lower than minum which is {}", LowerBound);
+      }
     }
-    if (newValue > this->upperBound) {
-      throw Error::make("value is higher than maximum which is {}", upperBound);
+
+    if constexpr (UpperBound < std::numeric_limits<T>::max()) {
+      if (UpperBound < newValue) {
+        throw Error::make("value is higher than maximum which is {}",
+                          UpperBound);
+      }
     }
+
     this->value = newValue;
   };
 
 protected:
   T value;
-  T lowerBound;
-  T upperBound;
 };
 
-/**
- * @brief A @Limited quantity, having infinity as upper bound
- */
-template <typename T> class LowerLimited : public Limited<T> {
-public:
-  LowerLimited(const T &lowerBound, const T &initialValue)
-      : Limited<T>(lowerBound, std::numeric_limits<T>::max(), initialValue){};
-
-  LowerLimited(const T &lowerBound) : LowerLimited(lowerBound, lowerBound){};
-};
-
-/**
- * @brief A @LowerLimited quantity, having 0.0 as lower bound
- */
-template <typename T> class Positive : public LowerLimited<T> {
-public:
-  Positive(const T &initialValue = static_cast<T>(0))
-      : LowerLimited<T>(static_cast<T>(0), initialValue){};
-};
-
-/**
- * @brief A @Limited quantity, having negative infinity as lower bound
- */
-template <typename T> class UpperLimited : public Limited<T> {
-public:
-  UpperLimited(const T &upperBound, const T &initialValue)
-      : Limited<T>(std::numeric_limits<T>::min(), upperBound, initialValue){};
-
-  UpperLimited(const T &upperBound) : UpperLimited(upperBound, upperBound){};
-};
+using Positive = Limited<float, 0.f, std::numeric_limits<float>::max()>;
 } // namespace mt_rrt
--- src/carpet/test/LimitedTests.cpp
+++ src/carpet/test/LimitedTests.cpp
@@ -6,50 +6,19 @@
 #include <memory>
 
 namespace {
-struct Limits {
-  float min;
-  float max;
-};
+using Lim = mt_rrt::Limited<float, 2.f, 5.f>;
 } // namespace
 
-using LimitedTestFixture = ::testing::TestWithParam<Limits>;
+TEST(LimitedTest, limited) {
 
-TEST_P(LimitedTestFixture, limited_on_both_sides) {
-  auto [min, max] = GetParam();
+  EXPECT_ANY_THROW(Lim{1.f});
+  EXPECT_ANY_THROW(Lim{6.f});
 
-  mt_rrt::Limited<float> limited(min, max);
+  EXPECT_NO_THROW(Lim{2.f});
+  EXPECT_NO_THROW(Lim{5.f});
 
-  limited.set(min);
-  EXPECT_EQ(limited.get(), min);
+  Lim val{3.f};
 
-  limited.set(max);
-  EXPECT_EQ(limited.get(), max);
-
-  const auto middle = 0.5f * (min + max);
-  limited.set(middle);
-  EXPECT_EQ(limited.get(), middle);
-}
-
-INSTANTIATE_TEST_CASE_P(LimitedTests, LimitedTestFixture,
-                        ::testing::Values(Limits{-3.f, 6.f}, Limits{3.f, 6.f},
-                                          Limits{-6.f, -3.f}));
-
-using LimitedNegativeTestFixture = ::testing::TestWithParam<Limits>;
-
-TEST_P(LimitedNegativeTestFixture, limited_on_both_sides_negative_tests) {
-  auto [min, max] = GetParam();
-
-  EXPECT_THROW(std::make_unique<mt_rrt::Limited<float>>(min, max),
-               mt_rrt::Error);
-}
-
-INSTANTIATE_TEST_CASE_P(LimitedNegativeTests, LimitedNegativeTestFixture,
-                        ::testing::Values(Limits{6.f, -3.f}, Limits{6.f, 3.f},
-                                          Limits{-3.f, -6.f}));
-
-TEST(LimitedTest, set_outside_bounds) {
-  mt_rrt::Limited<float> limited(-3.0, 6.0);
-
-  EXPECT_THROW(limited.set(-4.0), mt_rrt::Error);
-  EXPECT_THROW(limited.set(7.0), mt_rrt::Error);
+  EXPECT_NO_THROW(val.set(4.f));
+  EXPECT_ANY_THROW(Lim{1.f});
 }
